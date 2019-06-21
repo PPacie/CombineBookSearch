e6a75bc0222d02cf51868cf53af63891f311ac27
Pablo Paciello <ppacie@gmail.com>
2019-06-21 16:05:19 +0200

    Improve string concatenation in APIService.searchBy.

--- CombineBookSearch/Services/APIService.swift
+++ CombineBookSearch/Services/APIService.swift
@@ -19,7 +19,7 @@ enum APIService {
     
     static func searchBy(title: String) -> AnyPublisher<[Book], Error> {
         guard let titleForSearch = title.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed) else { fatalError("Cannot add %20 to search text") }
-        let params = titleForSearch + "+intitle:" +  titleForSearch + "&printType=books" + "&maxResults=40"
+        let params = titleForSearch.appending("+intitle:").appending(titleForSearch).appending("&printType=books&maxResults=40")
         var request = URLRequest(url: (URL(string: baseURL + params))!)
         request.httpMethod = "GET"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
